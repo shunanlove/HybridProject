a62a265aeb4a4f67e04cc8f7d637855112f9e3d4
macbook <yangshenglong@wityun.com>
2019-08-14 16:52:21 +0800

    分享

--- WebViewJsBridge/src/main/java/com/zxtnetwork/webviewjsbridge/utils/ShareUtils.java
+++ WebViewJsBridge/src/main/java/com/zxtnetwork/webviewjsbridge/utils/ShareUtils.java
@@ -1,9 +1,15 @@
 package com.zxtnetwork.webviewjsbridge.utils;
 
 import android.content.Context;
+import android.util.Log;
 
+import com.zxtnetwork.webviewjsbridge.listener.OneKeyShareCallback;
 import com.zxtnetwork.webviewjsbridge.model.ShareData;
 
+import java.util.HashMap;
+
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.PlatformActionListener;
 import cn.sharesdk.onekeyshare.OnekeyShare;
 
 /**
@@ -23,24 +29,26 @@ public class ShareUtils {
         //关闭sso授权
         oks.disableSSOWhenAuthorize();
         // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
-        oks.setTitle("标题");
+        oks.setTitle(shareData.getTitle());
         // titleUrl是标题的网络链接，仅在Linked-in,QQ和QQ空间使用
-        oks.setTitleUrl("http://sharesdk.cn");
+        oks.setTitleUrl(shareData.getTitleUrl());
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl(shareData.getUrl());
         // text是分享文本，所有平台都需要这个字段
-        oks.setText("我是分享文本");
+        oks.setText(shareData.getText());
         //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
-        oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
+//        oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
         // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
         //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
-        // url仅在微信（包括好友和朋友圈）中使用
-        oks.setUrl("http://sharesdk.cn");
-        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
-        oks.setComment("我是测试评论文本");
-        // site是分享此内容的网站名称，仅在QQ空间使用
-        oks.setSite("ShareSDK");
-        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
-        oks.setSiteUrl("http://sharesdk.cn");
 
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+//        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+//        oks.setSite("ShareSDK");
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+//        oks.setSiteUrl("http://sharesdk.cn");
+        //分享回调
+        oks.setCallback(new OneKeyShareCallback());
         //启动分享
         oks.show(context);
     }
