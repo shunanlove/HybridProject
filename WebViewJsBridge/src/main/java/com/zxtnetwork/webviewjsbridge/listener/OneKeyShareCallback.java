fd0f8dba51bf3b2b0f2fae695fcad923f2b7481e
Shu Nan <shunanlove@mail.com>
2019-08-15 15:38:47 +0800

    Revert "分享"
    This reverts commit 61e361ac

---
+++ WebViewJsBridge/src/main/java/com/zxtnetwork/webviewjsbridge/listener/OneKeyShareCallback.java
@@ -0,0 +1,29 @@
+package com.zxtnetwork.webviewjsbridge.listener;
+
+
+import java.util.HashMap;
+
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.PlatformActionListener;
+
+/**
+ * @Description: java类作用描述
+ * @Author: 大魔王老杨
+ * @Date: 2019-08-12 16:32
+ */
+public class OneKeyShareCallback implements PlatformActionListener {
+    @Override
+    public void onComplete(Platform platform, int i, HashMap<String, Object> hashMap) {
+
+    }
+
+    @Override
+    public void onError(Platform platform, int i, Throwable throwable) {
+
+    }
+
+    @Override
+    public void onCancel(Platform platform, int i) {
+
+    }
+}
